e0552c88ec0d4f4d9050f275238f4eb3eb959910
Christopher Kevin <mr.christopherkevin@gmail.com>
2023-05-16 16:09:23 +0700

    Yes

---
+++ framework/MaveFramework.framework/Headers/MaveFramework.h
@@ -0,0 +1,18 @@
+//
+//  MaveFramework.h
+//  MaveFramework
+//
+//  Created by Christopher Susandji on 11/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaveFramework.
+FOUNDATION_EXPORT double MaveFrameworkVersionNumber;
+
+//! Project version string for MaveFramework.
+FOUNDATION_EXPORT const unsigned char MaveFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaveFramework/PublicHeader.h>
+
+
